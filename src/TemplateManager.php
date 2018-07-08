1b0572cb057b65b86010cffe1ae65b7ce578a3af
enzomagro <enzomagro@live.fr>
2018-07-08 15:12:37 +0200

    add the set methods for placeholders: Destination content and link, Contains and User

--- src/TemplateManager.php
+++ src/TemplateManager.php
@@ -70,4 +70,83 @@ class TemplateManager
 
         return $text;
     }
+
+	/**
+     * Compute the summary text of the template object
+	 * @param string $text
+	 * @return string
+     */
+	public function setContainsSummary(&$text)
+	{
+		$quote = $this->_data['quote'];
+		$summary = [
+			PatternToReplace::$patternToReplace['quote_summary'],
+			PatternToReplace::$patternToReplace['quote_summary_html'],
+		];
+		$quoteFromRepository = QuoteRepository::getInstance()->getById($quote->id);
+		$text = str_replace(
+			$summary,
+			[$quote::renderText($quoteFromRepository), $quote::renderHtml($quoteFromRepository)],
+			$text
+		);
+
+		return $text;
+	}
+
+	/**
+     * Compute the destination link text of the template object
+	 * @param string $text
+	 * @return string
+     */
+	public function setDestinationLink(&$text)
+	{
+		$destination = DestinationRepository::getInstance()->getById($this->_data['quote']->destinationId);
+		$site = SiteRepository::getInstance()->getById($this->_data['quote']->siteId);
+		$quoteFromRepository = QuoteRepository::getInstance()->getById($this->_data['quote']->id);
+		$text = str_replace(
+			PatternToReplace::$patternToReplace['quote_destination_link'], 
+			$site->url . '/' . $destination->countryName . '/quote/' . $quoteFromRepository->id, 
+			$text
+		);
+
+		return $text;
+	}
+
+	/**
+     * Compute the destination name text of the template object
+	 * @param string $text
+	 * @return string
+     */
+	public function setDestinationName(&$text)
+	{
+		$destination = DestinationRepository::getInstance()->getById($this->_data['quote']->destinationId);
+		$text = str_replace(
+			PatternToReplace::$patternToReplace['quote_destination_name'],
+			$destination->countryName,
+			$text
+		);
+
+		return $text;
+	}
+
+	/**
+     * Compute the user text of the template object
+	 * @param string $text
+	 * @return string
+     */
+	public function setUser(&$text)
+	{
+		if(isset($this->_data['user'])  and ($this->_data['user']  instanceof User)){
+			$user = $this->_data['user'];
+		} else {
+			$user = ApplicationContext::getInstance()->getCurrentUser();
+		}
+		$text = str_replace(
+			PatternToReplace::$patternToReplace['user_first_name'],
+			ucfirst(mb_strtolower($user->firstname)),
+			$text
+		);
+
+		return $text;
+	}
 }
