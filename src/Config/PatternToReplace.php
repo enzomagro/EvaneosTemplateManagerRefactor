f43f05a421726d72459fb185a8a3b9ca24ce839e
enzomagro <enzomagro@live.fr>
2018-07-08 15:04:17 +0200

    add all the project files

---
+++ src/Config/PatternToReplace.php
@@ -0,0 +1,14 @@
+<?php
+
+class PatternToReplace 
+{
+	static $patternToReplace = [
+		'quote_destination_name' => '[quote:destination_name]',
+		'quote_destination_link' => '[quote:destination_link]',
+		'quote_summary' => '[quote:summary]',
+		'quote_summary_html' => '[quote:summary_html]',
+		'user_first_name' => '[user:first_name]',
+		'user_last_name' => '[user:first_name]',
+		'user_email' => '[user:email]',
+	];
+}
